5b7e51e3d3c368d7ccabb8e8a6a91117e47ff83d
Eric Liu <eliu23@uw.edu>
2023-05-23 16:12:31 -0700

    example fashion mnist tf

---
+++ setup.sh
@@ -0,0 +1,8 @@
+# Requires the latest pip
+pip install --upgrade pip
+
+# Current stable release for CPU and GPU
+pip install tensorflow
+
+# Install latest release for matplotlib
+python -m pip install -U matplotlib
