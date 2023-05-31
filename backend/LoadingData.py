706ae27e32c9ba85358c5cbf2fb9ea9da903bc82
Chris Storm <cxstorm@cs.washington.edu>
2023-05-31 13:27:27 -0700

    Progress on trying kaggle code

--- backend/LoadingData.py
+++ backend/LoadingData.py
@@ -8,3 +8,104 @@ from PIL import Image
 import os
 print(os.listdir("./input"))
 print(os.listdir("./input/car_data"))
+train_dir = "./input/car_data/train"
+test_dir = "./input/car_data/test"
+car_names_train = {}
+for i in os.listdir(train_dir):
+    car_names_train[i] = os.listdir(train_dir + '/' + i)
+
+car_images_ls = []
+car_names_ls = []
+car_classes = []
+car_directories = []
+
+for i in car_names_train:
+    car_classes.append(i)
+
+for i,j in enumerate(car_names_train.values()):
+    for img in j:
+        car_images_ls.append(img)
+        car_names_ls.append(car_classes[i])
+        
+for i in range(len(car_names_ls)):
+    car_directories.append(train_dir + '/' + car_names_ls[i] + '/' + car_images_ls[i])
+
+plt.imshow(Image.open(car_directories[1000]))
+plt.title(car_names_ls[1000])
+
+df = pd.DataFrame(data = [car_directories, car_names_ls], index = ["Directories", "Car Class"]).T
+df.head()
+df.to_csv('car_names_directories.csv', index = False)
+
+from keras.preprocessing.image import ImageDataGenerator
+from keras.applications import densenet
+from keras.models import Sequential, Model, load_model
+from keras.layers import Conv2D, MaxPooling2D, BatchNormalization
+from keras.layers import Activation, Dropout, Flatten, Dense
+from keras.callbacks import EarlyStopping, ReduceLROnPlateau, ModelCheckpoint, Callback
+from keras import regularizers
+
+#Pre-Defining some hyper-parameters
+img_width, img_height = 256, 256
+nb_train_samples = 8144
+nb_validation_samples = 8041
+epochs = 10
+steps_per_epoch = 256
+batch_size = 64
+n_classes = 196
+
+#Data Augmentation
+train_datagen = ImageDataGenerator(
+    rescale=1./ 255,
+    zoom_range=0.2,
+    rotation_range = 8,
+    horizontal_flip=True)
+
+test_datagen = ImageDataGenerator(rescale=1. / 255)
+
+train_generator = train_datagen.flow_from_directory(
+    train_dir,
+    target_size=(img_width, img_height),
+    batch_size=batch_size,
+    class_mode='categorical')
+
+validation_generator = test_datagen.flow_from_directory(
+    test_dir,
+    target_size=(img_width, img_height),
+    batch_size=batch_size,
+    class_mode='categorical')
+
+cnn = Sequential()
+cnn.add(Conv2D(filters = 16, kernel_size = (5,5), padding = 'same', activation = 'relu', input_shape = (256,256,3)))
+cnn.add(MaxPooling2D(pool_size = (2,2)))
+cnn.add(BatchNormalization(axis = 1))
+cnn.add(Dropout(0.22))
+cnn.add(Conv2D(filters = 32, kernel_size = (5,5), padding = 'same', activation = 'relu', input_shape = (256,256,3)))
+cnn.add(MaxPooling2D(pool_size = (2,2)))
+cnn.add(BatchNormalization(axis = 1))
+cnn.add(Dropout(0.22))
+cnn.add(Conv2D(filters = 64, kernel_size = (4,4), padding = 'same', activation = 'relu', input_shape = (256,256,3)))
+cnn.add(MaxPooling2D(pool_size = (2,2)))
+cnn.add(BatchNormalization(axis = 1))
+cnn.add(Dropout(0.2))
+cnn.add(Conv2D(filters = 96, kernel_size = (3,3), padding = 'same', activation = 'relu', input_shape = (256,256,3)))
+cnn.add(MaxPooling2D(pool_size = (2,2)))
+cnn.add(BatchNormalization(axis = 1))
+cnn.add(Flatten())
+cnn.add(Dropout(0.18))
+cnn.add(Dense(512, activation = 'relu'))
+cnn.add(BatchNormalization())
+cnn.add(Dense(512, activation = 'relu'))
+cnn.add(BatchNormalization())
+cnn.add(Dense(196, activation = 'sigmoid'))
+cnn.compile(optimizer = 'adam', loss = 'categorical_crossentropy', metrics = ['accuracy'])
+cnn.summary()
+
+model_history = cnn.fit(
+    train_generator,
+    epochs=epochs,
+    validation_data=validation_generator,
+    steps_per_epoch = steps_per_epoch,
+    validation_steps = nb_validation_samples // batch_size)
+
+cnn.save_weights('stanford_cars_folder_cnn_weights.h5')
